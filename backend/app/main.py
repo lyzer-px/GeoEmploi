86dbf75b3fc688aab4dd2abadc56d86175019219
rayan <rayan.ouerdane@epitech.eu>
2026-08-31 15:50:44 +0200

    feat: add database schema

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,2 +1,21 @@
-def main():
-    print("GeoEmploi")
+import logging
+
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from backend.app.api.routes import rooter
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.info("Startup...")
+    yield
+    logging.info("Shutting down...")
+
+app = FastAPI(
+    title="efficient token workflow engine API",
+    description="An API for building multi-agent AI workflows with optimized token usage.",
+    version="0.1.0",
+    lifespan=lifespan,
+)
+
+app.include_router(router=rooter)

c47aee2bdd30042d1788d7e187e777f5170ba06c
Alaric <alaric.fiou@epitech.eu>
2026-09-01 18:03:27 +0200

    feat: add a link to the login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './pages/Login';
+import { ROUTES } from './routes';
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
 
   return (
-    <h1> GEOEMPLOI</h1>
-  )
+    <div>
+      <h1>ChomageGO</h1>
+
+      <button onClick={() => navigate(ROUTES.LOGIN)}>
+        Aller vers la page de connexion
+      </button>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

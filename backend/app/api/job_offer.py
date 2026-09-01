ce263010503f23e98bc1716182241b03833f9ee4
Liam <liam.harfouche@epitech.eu>
2026-09-01 17:30:02 +0200

    feat(api): job offer post endpoint

---
+++ backend/app/api/job_offer.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+from fastapi import APIRouter
+
+from routes import router
+from app.schemas.user import JobOffer
+
+@router.post("/api/job-offer")
+async def create_job_offer(job_offer: JobOffer):
+    return {
+        "message": "Job offer created",
+        "title": job_offer.title,
+        "description": job_offer.description,
+        "recruiter": job_offer.recruiter_id
+    }

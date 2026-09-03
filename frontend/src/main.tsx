54d0e4650f67e13060b65b199ac6028d25a67603
Alaric <alaric.fiou@epitech.eu>
2026-09-03 14:54:54 +0200

    feat: change color theme and updates the packages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+import './index.css'
 import App from './App.tsx'
-
-startReactDsfr({ defaultColorScheme: "system" });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

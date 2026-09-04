3223dcccd3c1f4184de17eab3b9f0496c76a684e
Alaric <alaric.fiou@epitech.eu>
2026-09-04 10:27:14 +0200

    feat: remove unused import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import '../dsfr/dist/dsfr.min.css'
+//import '../dsfr/dist/dsfr.min.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

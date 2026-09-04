ff4ed84fd4d2cd01e87ac7cea5c97c0cabe6f5aa
Liam <liam.harfouche@epitech.eu>
2026-09-04 10:46:30 +0200

    feat(front): tileurl env var

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -2,11 +2,13 @@ import { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+const tileUrl = `${import.meta.env.VITE_TILE_URL}`;
+
 function Map() {
     useEffect(() => {
     const map = L.map("map").setView([46.6, 2.3], 6);
 
-    L.tileLayer("http://127.0.0.1:8000/tile/{z}/{x}/{y}.png", {
+    L.tileLayer(`${tileUrl}/{z}/{x}/{y}.png`, {
         attribution: "&copy; OpenStreetMap contributors",
     }).addTo(map);
 

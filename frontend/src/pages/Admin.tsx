ac94817f925f051495c0a727e3b3bdbdccd9e61f
Liam Lequet <liam.harfouche@epitech.eu>
2026-09-05 00:33:55 +0200

    feat: pie chart

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -12,7 +12,57 @@ function Admin() {
           <h1>Panneau d’administration</h1>
 
           <div className="admin-box">
-            {/* contenu du panneau ici */}
+            <div className="admin-dashboard">
+
+              <div className="admin-chart">
+                <div className="admin-chart-circle">
+                  <span>text</span>
+                </div>
+              </div>
+
+              <div className="admin-users">
+                <div className="admin-user">
+                  <div className="admin-user-avatar">◯</div>
+                  <div className="admin-user-info">
+                    <strong>Utilisateur 1</strong>
+                    <span>user1@email.com</span>
+                  </div>
+                </div>
+
+                <div className="admin-user">
+                  <div className="admin-user-avatar">◯</div>
+                  <div className="admin-user-info">
+                    <strong>Utilisateur 2</strong>
+                    <span>user2@email.com</span>
+                  </div>
+                </div>
+
+                <div className="admin-user">
+                  <div className="admin-user-avatar">◯</div>
+                  <div className="admin-user-info">
+                    <strong>Utilisateur 3</strong>
+                    <span>user3@email.com</span>
+                  </div>
+                </div>
+
+                <div className="admin-user">
+                  <div className="admin-user-avatar">◯</div>
+                  <div className="admin-user-info">
+                    <strong>Utilisateur 4</strong>
+                    <span>user4@email.com</span>
+                  </div>
+                </div>
+
+                <div className="admin-user">
+                  <div className="admin-user-avatar">◯</div>
+                  <div className="admin-user-info">
+                    <strong>Utilisateur 5</strong>
+                    <span>user5@email.com</span>
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </main>
